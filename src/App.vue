0e634c9b0a327ab99446fb5550e8652ec8664c43
marco.gattelli <marco.gattelli@gmail.com>
2022-05-03 09:57:52 +0200

    feat: Now you can use the input component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import CommonInput from '@/components/CommonInput/CommonInput.vue'
+import {Ref, ref, watch} from "vue";
+
+const model: Ref<string> = ref('')
+
+watch(model, (newValue) => {
+  console.log(newValue);
+})
 </script>
 
 <template>
+  <div class="h-full w-full p-8">
+    <div class="flex items-center justify-center">
+      <common-input v-model="model"/>
+    </div>
+  </div>
 </template>
---
+++ src/components/CommonInput/CommonInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <input type="text" class="rounded border px-2 py-1 w-80" :value="modelValue" @input="emits('update:modelValue', $event.target.value)" >
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: string
+  disabled?: boolean
+}
+const {modelValue} = defineProps<Props>()
+const emits = defineEmits<{(e: 'update:modelValue', modelValue: string): void}>()
+</script>
